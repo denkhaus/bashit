292379c664f7acf24fd36000e651452fac89b8a7
denkhaus <peristaltic@gmx.net>
2015-07-31 14:39:34 +0200

    autopush@1438346374

--- bash-it/bash_it.sh
+++ bash-it/bash_it.sh
@@ -63,15 +63,3 @@ unset config_file
 if [[ $PROMPT ]]; then
     export PS1=$PROMPT
 fi
-
-# Adding Support for other OSes
-PREVIEW="less"
-[ -s /usr/bin/gloobus-preview ] && PREVIEW="gloobus-preview"
-[ -s /Applications/Preview.app ] && PREVIEW="/Applications/Preview.app"
-
-# Load all the Jekyll stuff
-
-if [ -e $HOME/.jekyllconfig ]
-then
-  . $HOME/.jekyllconfig
-fi
